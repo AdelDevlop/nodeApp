061233c8514bfb998ab31f69dffe91d4a4e23289
Adel Chemache <adel.chemache16@gmail.com>
2022-09-28 00:01:47 +0200

    merge request auto  FINAL

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('OK ? 2 CI/CD FULLY ? REALLY ? ?')
+    res.send('OK ? 2 CI/CD FULLY ? REALLY ? ? FINAL')
 })
 
 app.listen(3000, console.log('server started at port 3000'))
