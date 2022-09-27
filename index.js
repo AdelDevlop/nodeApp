e7004f772752c10521c974b3d7fb7f70089fed20
Adel Chemache <adel.chemache16@gmail.com>
2022-09-28 01:46:19 +0200

    adding webhooh to update the new build

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', (req, res) => {
     res.send('COMPLETELY NEW VERSION !')
 })
 
-app.listen(3000, console.log('server started at port 3000'))
+app.get('/build/:build_number', (req, res) => {
+    console.log('received new build : ', req.params.build_number)
+    res.send('new build ? ' + req.params.build_number)
+})
+
+app.listen(3002, console.log('server started at port 3000'))
