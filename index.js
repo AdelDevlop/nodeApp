62587300b5e4166aba0dfa9edc9a1b20d47a79d0
Adel Chemache <adel.chemache16@gmail.com>
2022-09-28 14:41:21 +0200

    adding webhooh to update the new build step 6

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { stderr } = require('process')
 const shell = require('shelljs')
 
 app.get('/', (req, res) => {
-    res.send('Je pense que tout va bien !')
+    res.send('Je pense que tout va bien !!')
 })
 app.get('/build', (req, res) => {
     console.log('ok')
