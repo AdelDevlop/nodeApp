62dc745cefae15875508bf5a4a9b55d08443ac29
Adel Chemache <adel.chemache16@gmail.com>
2022-09-28 12:59:55 +0200

    adding webhooh to update the new build step 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+const { exec } = require('child_process')
+const { stderr } = require('process')
 
 app.get('/', (req, res) => {
     res.send('Je pense que tout va bien !')
@@ -9,7 +11,20 @@ app.get('/build', (req, res) => {
 })
 app.get('/build/:build_number', (req, res) => {
     console.log('received new build : ', req.params.build_number)
+
+    exec('docker ps -a', (err, stdout, stderr) => {
+        if ( err ) {
+            console.log('exec err ', err)
+            return
+        }
+        if ( stderr ) {
+            console.log('stderr err ', stderr)
+            return
+        }
+        console.log('exec stdout : ', stdout)
+    })
     res.send('new build ? ' + req.params.build_number)
+
 })
 
 app.listen(3002, console.log('server started at port 3000'))
